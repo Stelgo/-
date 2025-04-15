980037bcaf22bded5df9fb090d01f8255132620f
Stelgo <stelios.atp1000@gmail.com>
2025-04-15 19:00:49 +0300

    Add files via upload

---
+++ Lysari_Aferesi.py
@@ -0,0 +1,22 @@
+#Σημείωμα για εσάς, αγνοήστε τα comments σαν αυτό. 
+
+
+#Title here.
+print("Alpha 0.1.4")
+print("---------------------")
+def Lysi():
+    #variables for the problem are here.
+    AT = 100
+    AT_number = int(input("Αρχική τιμή: "))
+    E = int(input("Έκπτωση: "))
+    TT = None
+    #==================================#
+    #This the logic on  how to do said operation.
+    def Lysi_logic():
+        LYSI = AT - E
+        TT = LYSI
+        TT = TT * AT_number
+        TT = int(TT / AT)
+        print("Τελική τιμή είναι", TT)
+    Lysi_logic()
+Lysi()
