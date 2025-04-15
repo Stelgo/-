ac3f7b33db726c52812542edd07d62a672cadd92
Stelgo <stelios.atp1000@gmail.com>
2025-04-15 14:15:22 +0300

    Add files via upload

---
+++ Lysari_Prothesi.py
@@ -0,0 +1,18 @@
+#Title here:
+print("Alpha 0.1.4")
+print("---------------------")
+def Lysi():
+    #variables for the problem are here:
+    AT = 100
+    AT_number = int(input("Αρχική τιμή: "))
+    P = int(input("Πρόσθεση: "))
+    TT = None
+    #==================================#
+    def Lysi_logic():
+        LYSI = AT + P
+        TT = LYSI
+        TT = TT * AT_number
+        TT = int(TT / AT)
+        print("Τελική τιμή είναι", TT)
+    Lysi_logic()
+Lysi()
